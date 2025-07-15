2c1165a0575333820de980e85e0705a78d8afe02
Winson0330 <winsonw0330@gmail.com>
2025-07-15 13:36:01 -0400

    animations

--- C4GD Final Project/Assets/Scripts/PlayerController.cs
+++ C4GD Final Project/Assets/Scripts/PlayerController.cs
@@ -8,27 +8,35 @@ public class PlayerController : MonoBehaviour
     public static PlayerController instance;
     public float moveSpeed = 10;
     public bool onIsland;
+    Animator anim;
 
     void Start(){
         instance = this;
+        anim = GetComponent<Animator>();
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (onIsland){
-            float horizontalInput = Input.GetAxisRaw("Horizontal");
-            float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
+        if (horizontalInput < 0){
+            transform.localScale = new Vector3(-1, 1, 1);
+        } else if (horizontalInput > 0){
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (onIsland){
             Vector2 inputDir = new Vector2(horizontalInput, verticalInput).normalized;
 
             body.velocity = inputDir * moveSpeed;
         } else {
-            float horizontalInput = Input.GetAxisRaw("Horizontal");
-
             Vector2 inputDir = new Vector2(horizontalInput, 0);
 
             body.velocity = inputDir * moveSpeed;
         }
+
+        anim.SetBool("isRunning", body.velocity != Vector2.zero);
     }
 }
