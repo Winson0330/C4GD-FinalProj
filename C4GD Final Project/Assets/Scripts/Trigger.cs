e6e83f575840edfaf940794cd76a1cb65aa550b4
MatteoClementi1 <clementi.matteo@gmail.com>
2025-07-15 10:42:41 -0400

    trigger.cs
    trigger.cs

--- C4GD Final Project/Assets/Scripts/Trigger.cs
+++ C4GD Final Project/Assets/Scripts/Trigger.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeftRigger : MonoBehaviour
+public class Trigger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -15,4 +14,13 @@ public class LeftRigger : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            print("collision");
+        }
+    }
+
 }
