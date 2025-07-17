0486390075b879d9785d1002056b072f13f642f3
polylattice <oofoodden@gmail.com>
2025-07-17 16:43:46 -0400

    boat move

---
+++ C4GD Final Project/Assets/Scripts/ShipMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour
+{
+    public float rotationAmt=0;
+    public float rotationAmtTop=0;
+    public GameObject hull;
+    public GameObject flagPole;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        hull.transform.localEulerAngles=new Vector3(0,0,Mathf.Sin(Time.time*rotationAmt)*2.5f);
+        flagPole.transform.localEulerAngles=new Vector3(0,0,Mathf.Sin(Time.time*rotationAmt)*2f);
+    }
+}
